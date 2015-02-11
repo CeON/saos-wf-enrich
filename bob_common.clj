11a7245cf6bd19fbe6f0175530379474a35dcaaa
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-02-11 18:22:13 +0100

    Renaming files from kebab case to snake case

--- bob_common.clj
+++ bob_common.clj
@@ -1,4 +1,5 @@
 (require '[ clojure.string :as str])
+(require '[ me.raynes.fs :as fs])
 
 (def MOD-PATH "../../")
 
@@ -9,26 +10,21 @@
         (str/replace (re-pattern (str "^" old-prefix)) new-prefix)
               (str/replace (re-pattern (str old-suffix "$")) new-suffix)))
 
+(defn filter-files [ base-regexp fnames ]
+  (filter
+    #(re-find base-regexp (fs/base-name %))
+    fnames))
+
 ; TOOLS
 
 (def CLJ-CMD (str MOD-PATH "TOOLS/clj/sbin/clj"))
 
 ; INPUT FILES
 
-(def COMMO-COURT-JSON-FILES
-  (sort
-    (ls-cur-dir-with-path
-      (str MOD-PATH "get/json/out/") #"commo.*json.gz")))
-
-(def SUPRE-COURT-JSON-FILES
-  (sort
-    (ls-cur-dir-with-path
-      (str MOD-PATH "get/json/out/") #"supre.*json.gz")))
-
 (def ALL-COURT-JSON-FILES
-  (concat
-    COMMO-COURT-JSON-FILES
-    SUPRE-COURT-JSON-FILES))
+  (sort
+    (ls-cur-dir-with-path
+      (str MOD-PATH "get/json/out/") #".*json.gz")))
 
 ; OUTPUT FILES
 
@@ -45,31 +41,5 @@
 
 ;; CASE TAG
 
-(def CASE-TAG-SUPRE-COURT-JSON-FILES
-  (conv-inp-json-to-case-tag-files SUPRE-COURT-JSON-FILES))
-
-(def CASE-TAG-COMMO-COURT-JSON-FILES
-  (conv-inp-json-to-case-tag-files COMMO-COURT-JSON-FILES))
-
 (def CASE-TAG-ALL-COURT-JSON-FILES
   (conv-inp-json-to-case-tag-files ALL-COURT-JSON-FILES))
-
-;; PARTIES-TAG
-
-(def PARTIES-TAG-COMMO-COURT-JSON-FILES
-  (map
-     #(conv-fname %
-        "../../get/json/out/"
-        "../../parties/tags/out/tags"
-        ".json.gz" "_parties_tag.json.gz")
-     COMMO-COURT-JSON-FILES))
-
-;; LAW-LINKS-TAG
-
-(def LAW-LINKS-TAG-ALL-COURT-JSON-FILES
-  (map
-     #(conv-fname %
-        "../../get/json/out/"
-        "../../law-links/tags/out/tags"
-        ".json.gz" "_law_links_tag.json.gz")
-     ALL-COURT-JSON-FILES))
