a5a470cb15f2d47a31211907f6d77c2747e9e9b9
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-02-11 12:16:03 +0100

    Refactoring namespace naming for imports in get_json.clj

--- get/json/get_json.clj
+++ get/json/get_json.clj
@@ -1,5 +1,5 @@
-(use 'clj.rest-get :reload)
 (require '[cheshire.core :as cc])
+(require '[clj.rest-get :as get])
 
 (def court-type->out-fname-format
   { "COMMON"  "out/commo_court_%04d.json.gz"
@@ -7,9 +7,9 @@
     "CONSTITUTIONAL_TRIBUNAL" "out/const_tribu_%04d.json.gz"
     "NATIONAL_APPEAL_CHAMBER" "out/appea_chamb_%04d.json.gz"})
 
-(defn main []
-  (fetch-buffer-all
+(defn run []
+  (get/fetch-buffer-all
     "https://saos-test.icm.edu.pl/api/dump/judgments?pageSize=100&pageNumber=0"
     court-type->out-fname-format))
 
-(main)
+(run)
