d744aa4a3deda0c5dcc7d0db27518e3772991edb
Michal Lopuszynski <lopusz@gmail.com>
2015-02-13 15:25:21 +0000

    Adding IPv6 switches to JVM_OPTS in clj and get_json scripts

--- get/rest/get_json.sh
+++ get/rest/get_json.sh
@@ -7,5 +7,5 @@ if [ ! -d out ]; then
     mkdir out
 fi
 
-JVM_OPTS=-Xmx2g
+JVM_OPTS="-Xmx2g -Djava.net.preferIPv6Addresses=true"
 java ${JVM_OPTS} -ea -cp "$CLOJURE_JAR" clojure.main ./get_json.clj
