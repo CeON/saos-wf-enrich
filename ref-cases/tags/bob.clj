88958de923d38fa8ef5f6df5a6e3c35f8a05e5cb
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-02-13 17:12:06 +0100

    Adding processing groups of files to ref-cases module

--- ref-cases/tags/bob.clj
+++ ref-cases/tags/bob.clj
@@ -2,7 +2,9 @@
 
 (load-clj-file "../../bob_common.clj")
 
-(defn gen-stat-rules [ court-type script switch out-suffix ]
+(def N 20)
+
+(defn gen-stat-rules [court-type script switch out-suffix]
   (let [
          court-type-re
            (if (= court-type "every_court")
@@ -16,17 +18,25 @@
     [ [ (file CLJ-CMD) (inp script) switch (inp inp-files)
           ">" (out out-file) ]]))
 
+(defn gen-ref-cases-tags-rule [judgment-files tag-files]
+  [ (file CLJ-CMD)
+    (inp "./gene_ref_cases_tags.clj")
+    (inp REF-CASES-DICT-FILE)
+    (inp judgment-files)
+    (out tag-files) ])
+
+(defn gen-partitioned-rules [judgment-files tag-files]
+ (map gen-ref-cases-tags-rule
+   (partition-all N judgment-files)
+   (partition-all N tag-files)))
+
 (defrule
   (concat
-     [[ (file CLJ-CMD)
-        (inp "./gene_ref_cases_tags.clj")
-        (inp REF-CASES-DICT-FILE)
-        (inp EVERY-COURT-FILES)
-        (out REF-CASES-TAG-FILES) ]]
-        (mapcat
-          #(concat
-             (gen-stat-rules % "./gene_ref_cases_stat_sum.clj" "" "_sum.txt")
-             (gen-stat-rules % "./gene_ref_cases_stat_frq.clj" "" "_frq.txt")
-             (gen-stat-rules % "./gene_ref_cases_stat_frq.clj" "-r" "_frq_resolved.txt")
-             (gen-stat-rules %"./gene_ref_cases_stat_ids.clj" "" "_ids.txt"))
-             [ "every_court" "commo_court" "supre_court" "const_tribu" "appea_chamb" ])))
+    (gen-partitioned-rules  EVERY-COURT-FILES REF-CASES-TAG-FILES)
+    (mapcat
+      #(concat
+         (gen-stat-rules % "./gene_ref_cases_stat_sum.clj" "" "_sum.txt")
+         (gen-stat-rules % "./gene_ref_cases_stat_frq.clj" "" "_frq.txt")
+         (gen-stat-rules % "./gene_ref_cases_stat_frq.clj" "-r" "_frq_resolved.txt")
+         (gen-stat-rules %"./gene_ref_cases_stat_ids.clj" "" "_ids.txt"))
+         [ "every_court" "commo_court" "supre_court" "const_tribu" "appea_chamb" ])))
