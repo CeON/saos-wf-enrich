edef18da2451ef7965a44431f1ac2578cfedaabb
Michal Lopuszynski <lopusz@gmail.com>
2015-02-27 23:23:38 +0000

    Adding control over number of cpus used by querying tool

--- TOOLS/clj/src/clj/query.clj
+++ TOOLS/clj/src/clj/query.clj
@@ -1,6 +1,9 @@
 (ns clj.query
-  (require [ squeezer.core :as sqc]
+  (require
+           [ clojure.string :as str]
+           [ squeezer.core :as sqc]
            [ cheshire.core :as cc]
+           [ paralab.core :as pc]
            [ paralab.fj-core :as fjc]
            [ paralab.fj-tasks :as fjt])
   (:gen-class))
@@ -12,13 +15,47 @@
 (defn print-plain [data]
   (doall (map #(println %) data)))
 
-(defn -main
-  [ & args ]
+(defn keys-sorted-by-val-desc [ m ]
+  (sort-by m #(compare %2 %1) (keys m)))
+
+(defn print-freq-desc [ freq ]
+  (doall
+    (map
+       #(println (format "%10d %s"(get freq %) %))
+       (keys-sorted-by-val-desc freq))))
+
+(defn is-n-cpu-opt? [opt]
+  (some? (re-matches #"-p[0-9]*" opt)))
+
+(defn get-n-cpus [ opt ]
   (let [
-         { :keys [transform-f merge-f print-f]
+         tot-n-cpus
+           (pc/get-n-cpus)
+         n-cpus-str
+           (str/replace opt #"-p" "")
+         n-cpus
+           (if (empty? n-cpus-str)
+             tot-n-cpus
+             (Integer. n-cpus-str))
+        ]
+       n-cpus))
+
+;; -p1 is the only way to enable serial computation
+
+(defn -main [ & args ]
+  (let [
+         [ n-cpus query-fname data-fnames ]
+           (if (is-n-cpu-opt? (first args))
+             [ (get-n-cpus (first args))
+               (second args)
+               (into [] (drop 2 args)) ]
+             [ (pc/get-n-cpus)
+               (first args)
+               (into [] (rest args)) ])
+        { :keys [transform-f merge-f print-f]
             :or { merge-f concat
                   print-f print-json }}
-            (load-file (first args))
+            (load-file query-fname)
          transform-f*
            (fn [fname]
              (let [
@@ -26,17 +63,17 @@
                             (sqc/reader-compr fname) true)
                   ]
                (transform-f data)))
-         fnames
-           (into [] (rest args))
          fj-task
            (fjt/make-fj-task-map-reduce-vec
-                 :data fnames
+                 :data data-fnames
                  :map-f transform-f*
                  :reduce-f merge-f
                  :size-threshold 2)
-         fj-pool
-           (fjc/make-fj-pool)
          res
-           (fjt/run-fj-task fj-pool fj-task)
+           (if (> n-cpus 1)
+             (fjt/run-fj-task
+               (fjc/make-fj-pool n-cpus)
+               fj-task)
+             (fjt/run-fj-task-serial fj-task))
         ]
      (print-f res)))
