33390d5bbeedf56c55b069938841b34d3fc5500a
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-03-18 18:06:16 +0100

    Improving alimony records selection

--- ana-alimn/data/q_alimony.clj
+++ ana-alimn/data/q_alimony.clj
@@ -1,9 +1,10 @@
+(require '[clojure.string :as str])
 (require '[clj.query :refer :all])
 
 (def monster-regexp #"p> *o +podwyższenie +alimentów|p> *o +alimenty|p> *o +obniżenie +alimentów|p> *o +uchylenie +obowiązku +alimentacyjnego|p> *o +ustalenie +wygaśnięcia +obowiązku +alimentacyjnego|p> *o +wygaśnięcie +obowiązku +alimentacyjnego|p> *o +uchylenie +alimentów|p> *o +zasądzenie +alimentów|p> *o +ustalenie, +że +obowiązek +alimentacyjny +ustał|p> *o +ustalenie, +że +obowiązek +alimentacyjny +wygasł|p> *o +ustalenie +ojcostwa +i +alimenty|p> *o +zmianę +orzeczenia +w +zakresie +alimentów|p> *o +ustalenie +ustania +obowiązku +alimentacyjnego|p> *o +zmianę +obowiązku +alimentacyjnego")
 
 (def small-regexp
-  #"(?i)p> *o +podwyższenie +alimentów|p> *o +alimenty|p> *o +obniżenie +alimentów")
+  #"(?i)p>\s*o\s+podwyższenie\s+alimentów|p>\s*o\s+alimenty|p>\s*o\s+obniżenie\s+alimentów")
 
 ;(defn refers-to-alimony? [j]
 ;  (re-find #"(?i)aliment[^a]|^aliment[^a]" (:textContent j)))
@@ -11,9 +12,20 @@
 ;(defn refers-to-alimony? [j]
 ;  (re-find #"(?i)obowiąz[^ ]* +aliment" (:textContent j)))
 
+(defn remove-justification [s]
+  (first
+    (str/split s #"(?i)<h2>UZASADNIENIE</h2>")))
+
+(defn get-reason [s]
+  (as->
+    s ---
+    (re-find #"(?i)p>\s*o [^<]*" ---)
+    (str ---)))
+
 (defn refers-to-alimony? [j]
   (and (= "SENTENCE" (:judgmentType j))
-       (re-find small-regexp (:textContent j))))
+       (re-find small-regexp
+         (get-reason (remove-justification (:textContent j))))))
 
 {
    :transform-f
