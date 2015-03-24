8e1031a43a765c299d5b33d503e38e5de9ba08d2
Michal Lopuszynski <lopusz@gmail.com>
2015-03-24 11:57:15 +0000

    Adding skeleton of runner script

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+date +"[LOG] %Y-%m-%d %H:%M Started analysis." 
+
+START_DIR=$(dirname $0)
+
+cd ${START_DIR}
+
+rm -f bob.log get.log
+
+cd get/rest
+rm -rf out
+./get_json.sh > ../../get.log 2>&1
+
+if [ $? == 0 ]; then
+    date +"[LOG] %Y-%m-%d %H:%M Success fetching data."
+    cd ../../
+    bob cl -f > bob.log 2>&1
+    bob bu -c3 >> bob.log 2>&1
+    
+    if [ $? == 0 ]; then
+        date +"[LOG] %Y-%m-%d %H:%M Success processing data."
+    else
+        date +"[LOG] %Y-%m-%d %H:%M Failed processing data."
+    fi
+else 
+    date +"[LOG] %Y-%m-%d %H:%M Failed fetching data."    
+    cd ../../
+fi
