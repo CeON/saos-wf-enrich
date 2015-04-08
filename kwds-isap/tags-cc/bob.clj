e80a5c2bd2b2ae2ab7c04434f665874002eff564
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-04-08 17:25:19 +0200

    Adding html report generation (kwds-isap)

--- kwds-isap/tags-cc/bob.clj
+++ kwds-isap/tags-cc/bob.clj
@@ -25,16 +25,25 @@
     (inp judgment-files)
     (out tag-files) ])
 
+(defn gen-ref-cases-html-rule [ tag-json-files html-files]
+  [ (file CLJ-CMD)
+    (inp GEN-KWDS-ISAP-HTML-CLJ)
+    (inp tag-json-files)
+    (out html-files) ])
+
 (defrule
   (concat
     (gen-partitioned-rules
        N gen-ref-cases-tags-rule
-       COMMO-COURT-FILES KWDS-ISAP-TAG-FILES)
+       COMMO-COURT-FILES KWDS-ISAP-CC-TAG-FILES)
+    (gen-partitioned-rules
+       N gen-ref-cases-html-rule
+       KWDS-ISAP-CC-TAG-FILES KWDS-ISAP-CC-HTML-FILES)
     [ [ (file CLQ-CMD) "-p1"
           (inp "./q_kwds_isap_stat_sum.clj")
-          (inp KWDS-ISAP-TAG-FILES)
+          (inp KWDS-ISAP-CC-TAG-FILES)
           ">" (out "out/stat/kwds_isap_sum.txt") ]
       [ (file CLQ-CMD) "-p1"
           (inp "./q_kwds_isap_stat_frq.clj")
-          (inp KWDS-ISAP-TAG-FILES)
+          (inp KWDS-ISAP-CC-TAG-FILES)
           ">" (out "out/stat/kwds_isap_frq.txt") ] ]))
